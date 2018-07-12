b60b43a2119abf0ab52b8c7dfc89e2e7bbd24280
c <c@dai2>
2018-07-11 22:17:35 -0600

    'Fixed' another compiler error.

--- vector/jvm/src/main/scala/ai/dragonfly/math/vector/VectorN.scala
+++ vector/jvm/src/main/scala/ai/dragonfly/math/vector/VectorN.scala
@@ -5,43 +5,7 @@ import js.JSConverters._
 import scala.scalajs.js.annotation.{JSExport, JSExportAll, JSExportTopLevel}
 
 
-object VectorN {
 
-  def fill(dimension: Int, value:Double): VectorN = new VectorN(Array.fill[Double](dimension)(value))
-
-  def random(dimension: Int, maxNorm:Double = 1.0): VectorN = new VectorN(Array.fill[Double](dimension)(Math.random()*maxNorm))
-
-  def midpoint(v0: VectorN, v1: VectorN): VectorN = {
-    if (v0.dimension != v1.dimension) throw MismatchedVectorDimensionsException(
-      s"midpoint undefined on vectors with different dimensions:\n" +
-        s"dim($v0) = ${v0.dimension}\n" +
-        s"dim($v1) = ${v1.dimension}"
-    )
-    val midpointValues = new Array[Double](v0.values.length)
-    for (i <- 0 until v0.dimension) { midpointValues(i) = (v0(i) + v1(i)) / 2.0 }
-    new VectorN(midpointValues)
-  }
-
-  def average(vectors: Array[VectorN]): Vector = {
-    val l = vectors(0).dimension
-    for(i <- 1 until vectors.length) {
-      if (vectors(i).dimension != l) throw MismatchedVectorDimensionsException(
-        s"average undefined on vectors with different dimensions:\n" +
-          s"dim(${vectors(0)}}) = $l\n" +
-          s"dim(${vectors(i)}) = ${vectors(i).dimension}"
-      )
-    }
-
-    val avg = new VectorN(new Array[Double](l))
-    for (vector <- vectors) {
-      avg.add(vector)
-    }
-    avg.scale(1.0/l)
-  }
-
-  def average(vectors: js.Array[VectorN]): Vector = average(vectors.toArray)
-
-}
 
 /**
  * Created by clifton on 1/9/17.
--- vector/shared/src/main/scala/ai/dragonfly/math/stats/TestStreamingStats.scala
+++ vector/shared/src/main/scala/ai/dragonfly/math/stats/TestStreamingStats.scala
@@ -10,19 +10,19 @@ import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("test.TestStreamingStats")
 object TestStreamingStats {
-  @JSExport def test0(): Unit = {
-    val k = GaussianKernel(5)
-
-    val vectorStats = new StreamingVectorStats(5)
-    for (i <- 0 until 100) {
-      val rVector = VectorN.random(5, 2)
-      vectorStats(rVector, k.weight(rVector))
-    }
-    println(vectorStats)
-
-    println("Average: " + vectorStats.average())
-    println("Variance: " + vectorStats.variance())
-    println("Standard Deviation: " + vectorStats.standardDeviation())
-  }
+//  @JSExport def test0(): Unit = {
+//    val k = GaussianKernel(5)
+//
+//    val vectorStats = new StreamingVectorStats(5)
+//    for (i <- 0 until 100) {
+//      val rVector = Vector.random(5, 2)
+//      vectorStats(rVector, k.weight(rVector))
+//    }
+//    println(vectorStats)
+//
+//    println("Average: " + vectorStats.average())
+//    println("Variance: " + vectorStats.variance())
+//    println("Standard Deviation: " + vectorStats.standardDeviation())
+//  }
 
 }
