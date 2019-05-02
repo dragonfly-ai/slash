53db4bf9ac67fc342b7d3f42609b5151d58f8367
c <c@dragonfly.ai>
2019-05-02 16:57:26 -0600

    fixed directory structure error.

--- vector/jvm/src/main/scala/ai/dragonfly/math/vector/native/VectorTests.scala
+++ vector/jvm/src/main/scala/ai/dragonfly/math/vector/native/VectorTests.scala
@@ -1,8 +1,8 @@
 package ai.dragonfly.math.vector.native
 
 /**
- * Created by clifton on 1/9/17.
- */
+  * Created by clifton on 1/9/17.
+  */
 
 object VectorTests {
   def main(args: Array[String]): Unit = ai.dragonfly.math.vector.VectorTests.apply()
