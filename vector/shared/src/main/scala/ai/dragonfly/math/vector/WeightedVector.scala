6da5d014d0cfb1ab19ea21bcc7563d622458a6d1
c <sourcerer@dragonfly.ai>
2022-03-21 12:05:01 -0600

    Increased Version to 0.4525
    Refined WeightedVector and extended its demonstrations.
      exposed def weight:Double
      removed redundant apply method.
      changed order of factory parameters

--- vector/shared/src/main/scala/ai/dragonfly/math/vector/WeightedVector.scala
+++ vector/shared/src/main/scala/ai/dragonfly/math/vector/WeightedVector.scala
@@ -3,24 +3,26 @@ package ai.dragonfly.math.vector
 import ai.dragonfly.math.example.Demonstrable
 
 object WeightedVector extends Demonstrable {
-  def apply(weight:Double, vector:Vector):WeightedVector = {
-    new WeightedVector(vector).addWeight(weight)
-  }
 
   override def demo(implicit sb:StringBuilder = new StringBuilder()):StringBuilder = {
-    val wv0 = WeightedVector(0.5, Vector3(1.1, 2.5, 0.1))
-    sb.append(s"\tWeightedVector: $wv0")
+    val wv0 = WeightedVector(Vector(1.1, 2.5, 0.1), 0.5)
+    sb.append(s"\tWeightedVector: $wv0\n")
     sb.append(s"\tWeightedVector.weighted: ${wv0.weighted}\n")
+    sb.append(s"\tWeightedVector.weight: ${wv0.weight}\n")
+    sb.append(s"\tWeightedVector.addWeight(0.25): ${wv0.addWeight(0.25)}\n")
+    sb.append(s"\tWeightedVector: $wv0\n")
+    sb.append(s"\tWeightedVector.weighted: ${wv0.weighted}\n")
+    sb.append(s"\tWeightedVector.weight: ${wv0.weight}\n")
   }
 
   override def name: String = "WeightedVector"
 }
 
-case class WeightedVector(unweighted: Vector) {
-  private var weight:Double = 0.0
-  def addWeight(w: Double): WeightedVector = {
-    weight = weight + w
+case class WeightedVector(unweighted: ai.dragonfly.math.vector.Vector, private var w: Double = 0.0) {
+  def weight:Double = w
+  def addWeight(w1: Double): WeightedVector = {
+    w = w + w1
     this
   }
-  def weighted: Vector = unweighted * weight
+  def weighted: ai.dragonfly.math.vector.Vector = unweighted * weight
 }
