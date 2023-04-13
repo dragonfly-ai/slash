85fe46fde18c7ab32d51dc7c53514d1027037cea
c <git@actorgrid.net>
2023-04-13 14:54:59 -0600

    Added headers.

--- vector/shared/src/main/scala/ai/dragonfly/math/interval/Interval.scala
+++ vector/shared/src/main/scala/ai/dragonfly/math/interval/Interval.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 dragonfly.ai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ai.dragonfly.math.interval
 
 import ai.dragonfly.math.Random.defaultRandom
@@ -7,7 +23,6 @@ import math.Numeric.Implicits.infixNumericOps
 
 object Interval {
   import ai.dragonfly.math.Random.*
-
   private inline def between(i:Interval[Int], r:scala.util.Random) = r.between(i.min, i.MAX)
   private inline def between(i:Interval[Long], r:scala.util.Random) = r.between(i.min, i.MAX)
   private inline def between(i:Interval[BigInt], r:scala.util.Random) = r.between(i.min, i.MAX)
