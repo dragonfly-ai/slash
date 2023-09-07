bb9c2bebc7dd01b0aaf52efe36e9e7badddc8dd8
c <git@actorgrid.net>
2023-09-07 12:24:56 -0600

    Fixed off by one bug in the Gamma.lnGamma function. Removed unused import in GaussianDemo.scala

--- demo/shared/src/main/scala/ai/dragonfly/math/stats/probability/distributions/stream/GaussianDemo.scala
+++ demo/shared/src/main/scala/ai/dragonfly/math/stats/probability/distributions/stream/GaussianDemo.scala
@@ -3,10 +3,7 @@ package ai.dragonfly.math.stats.probability.distributions.stream
 import ai.dragonfly.math.OnlineProbDistDemo
 import ai.dragonfly.math.stats.*
 import probability.distributions
-import jdk.jfr.Frequency
-
-import scala.util.Random
 
 object GaussianDemo {
-  val demo = OnlineProbDistDemo[Double, distributions.Gaussian, Gaussian]("Streaming Gaussian", distributions.Gaussian(42.0, 7.0), Gaussian(), 1000)
+  val demo: OnlineProbDistDemo[Double, distributions.Gaussian, Gaussian] = OnlineProbDistDemo[Double, distributions.Gaussian, Gaussian]("Streaming Gaussian", distributions.Gaussian(42.0, 7.0), Gaussian(), 1000)
 }
